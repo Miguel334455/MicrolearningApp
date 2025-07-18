0012320c94ebc5f415c6f7415c6f7019cdf28b85
Miguel334455 <mangelramirez.rp@gmail.com>
2025-07-18 17:57:12 -0500

    backend

---
+++ Back/Back/Models/Courses.cs
@@ -0,0 +1,11 @@
+﻿namespace Back.Models
+{
+    public class Courses
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int IdCategory { get; set; }
+
+    }
+}
